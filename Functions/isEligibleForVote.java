6f342bb1f07502b6ace15a3682810fc4845c0090
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2023-12-21 22:47:42 +0530

    Add files via upload

---
+++ Functions/isEligibleForVote.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class isEligibleForVote {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Age : ");
+        int age = sc.nextInt();
+        String result = "";
+        if (isEligible(age)) {
+            result = "is";
+        } else {
+            result = "isn't";
+        }
+        System.out.println("The Person " + result + " Eligible for Vote!");
+    }
+
+    public static boolean isEligible(int age) {
+        return age >= 18;
+    }
+}
