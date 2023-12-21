9043e5dfdf7f2f9b38a25982c2d05a9f2027ca12
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2023-12-21 23:10:30 +0530

    Add files via upload

---
+++ Functions/Sum_Of_Two_Numbers.java
@@ -0,0 +1,19 @@
+package KunalKushWaha;
+
+import java.util.Scanner;
+
+public class Sum_Of_Two_Numbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the First Number : ");
+        int a = sc.nextInt();
+        System.out.print("Enter the Second Number : ");
+        int b = sc.nextInt();
+        int sum = calculateSumOfTwo(a, b);
+        System.out.println("Result is : " + a + " + " + b + " = " + sum);
+    }
+
+    public static int calculateSumOfTwo(int a, int b) {
+        return a + b;
+    }
+}
