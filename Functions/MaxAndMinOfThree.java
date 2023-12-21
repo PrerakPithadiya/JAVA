8027d07803ceb539c91fbb5132fcbd9b16b114b9
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2023-12-21 22:23:04 +0530

    Add files via upload

--- Functions/MaxAndMinOfThree.java
+++ Functions/MaxAndMinOfThree.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
 
+public class MaxAndMinOfThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the First Number : ");
+        int a = sc.nextInt();
+        System.out.print("Enter the Second Number : ");
+        int b = sc.nextInt();
+        System.out.print("Enter the Third Number : ");
+        int c = sc.nextInt();
+        int max = maxOfThree(a, b, c);
+        int min = minOfThree(a, b, c);
+        System.out.println("The Maximum of Three Numbers is : " + max);
+        System.out.println("The Minimum of Three Numbers is : " + min);
+    }
+
+    public static int maxOfThree(int a, int b, int c) {
+        return Math.max(Math.max(a, b), c);
+    }
+
+    public static int minOfThree(int a, int b, int c) {
+        return Math.min(Math.min(a, b), c);
+    }
+}
