4fb83cf1c3e92efd74a904c57304c83e4204291b
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2024-01-10 15:39:22 +0530

    Add files via upload

---
+++ Functions/Binomial_Coefficient.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Binomial_Coefficient {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Value of 'n' : ");
+        int n = sc.nextInt();
+        System.out.print("Enter the Value of 'r' : ");
+        int r = sc.nextInt();
+        int binCoeff = calculateBinomialCoefficient(n, r);
+        System.out.print("The Binomial Coefficient is : ");
+        System.out.println(binCoeff);
+    }
+
+    public static int calculateBinomialCoefficient(int n, int r) {
+        int n_fact = factorial(n);
+        int r_fact = factorial(r);
+        int n_r_fact = factorial(n - r);
+        return n_fact / (r_fact * n_r_fact);
+    }
+
+    public static int factorial(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+}
