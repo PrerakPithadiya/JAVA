c63e6d6a6fca3b14ae43dcc7e7219a9a6fcea6d5
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2024-01-11 22:49:26 +0530

    Add files via upload

---
+++ Functions/Sum_Of_Given_Series.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Sum_Of_Given_Series {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number of Terms : ");
+        int n = sc.nextInt();
+        int sum = calculateSumOfSeries(n);
+        System.out.println("The Sum of Series is : " + sum);
+    }
+
+    public static int calculateSumOfSeries(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += (factorial(i) / i);
+        }
+        return sum;
+    }
+
+    public static int factorial(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+}
