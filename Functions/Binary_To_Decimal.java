b884c0e898922f0081d15a4ffb5f25342efd03b6
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2024-01-22 21:24:13 -0800

    Add files via upload

---
+++ Functions/Binary_To_Decimal.java
@@ -0,0 +1,23 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class Binary_To_Decimal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number : ");
+        int n = sc.nextInt();
+        int decNum = convertToDecimal(n);
+        System.out.println("Decimal of " + n + " is : " + decNum);
+    }
+
+    public static int convertToDecimal(int binNum) {
+        int pow = 0, decNum = 0;
+        while (binNum > 0) {
+            int lastDigit = binNum % 10;
+            decNum += (lastDigit * (int) Math.pow(2, pow++));
+            binNum /= 10;
+        }
+        return decNum;
+    }
+}
