f0994b5f7dacfa3ca22cbd1c1fbfa154b1170ec0
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2024-02-22 12:10:46 +0530

    Add files via upload

---
+++ Functions/Decimal_To_Octal.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Decimal_To_Octal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rd = new Random();
+        System.out.print("Enter the Decimal Number : ");
+        int n = sc.nextInt();
+        int octNum = toOctal(n);
+        System.out.println("Octal Number is : " + octNum);
+    }l
+
+    public static int toOctal(int decNum) {
+        int pow = 0, octNum = 0;
+        while (decNum != 0) {
+            int r = decNum % 8;
+            octNum += (r * Math.pow(10, pow++));
+            decNum /= 8;
+        }
+        return octNum;
+    }
+}
