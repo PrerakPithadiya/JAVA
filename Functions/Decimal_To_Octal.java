1b36af239f2e20def9aa161dd52bcaf03d851bde
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2024-02-22 12:40:30 +0530

    Update Decimal_To_Octal.java

--- Functions/Decimal_To_Octal.java
+++ Functions/Decimal_To_Octal.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Decimal_To_Octal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Random rd = new Random();
         System.out.print("Enter the Decimal Number : ");
         int n = sc.nextInt();
         int octNum = toOctal(n);
