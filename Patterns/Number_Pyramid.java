23584a7fdfdd65bd8b8e6c2d629801208038ab12
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2024-03-29 16:26:08 +0530

    Add files via upload

---
+++ Patterns/Number_Pyramid.java
@@ -0,0 +1,17 @@
+package Patterns;
+
+public class Number_Pyramid {
+    public static void main(String[] args) {
+        int n = 7;
+        for (int i = 1; i <= n; i++) {
+            int a = 1;
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(a++ + " ");
+            }
+            System.out.println();
+        }
+    }
+}
