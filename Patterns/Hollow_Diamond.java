8edc81a72d26c2d60e341f3b6fdf33857a8b2b01
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2024-03-29 16:18:18 +0530

    Add files via upload

--- Patterns/Hollow_Diamond.java
+++ Patterns/Hollow_Diamond.java
@@ -1,1 +1,29 @@
+package Patterns;
 
+public class Hollow_Diamond {
+    public static void main(String[] args) {
+        int n = 7;
+        int nsp = n - 1;
+        int nst = 1;
+        for (int i = 1; i <= 2 * n - 1; i++) {
+            for (int j = 1; j <= nsp; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= nst; j++) {
+                if (j == 1 || j == nst) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            if (i < n) {
+                nsp--;
+                nst++;
+            } else {
+                nsp++;
+                nst--;
+            }
+            System.out.println();
+        }
+    }
+}
