1c0fd5f0bc5daf2ed6f0f4f1f1f9ec5162416c30
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2024-03-29 21:07:44 +0530

    Update Number_Diamond.java

--- Patterns/Number_Diamond.java
+++ Patterns/Number_Diamond.java
@@ -2,32 +2,32 @@ package Patterns;
 
 public class Number_Diamond {
     public static void main(String[] args) {
-        int rows = 5;               // Number of rows (can be adjusted)
-        int spaces = 0;             // Number of spaces
-        int stars = 2 * rows - 1;   // Number of stars
-        int currentValue = rows;    // Value to be printed
+        int numberOfRows = 5;               // Number of rows (can be adjusted)
+        int numberOfSpaces = 0;             // Number of spaces
+        int numberOfStars = 2 * numberOfRows - 1;     // Number of stars
+        int currentValue = numberOfRows;               // Value to be printed
 
         // Loop through each row
-        for (int i = 1; i <= 2 * rows - 1; i++) {
+        for (int i = 1; i <= 2 * numberOfRows - 1; i++) {
             // Print leading spaces
-            for (int j = 1; j <= spaces; j++) {
+            for (int j = 1; j <= numberOfSpaces; j++) {
                 System.out.print("  ");
             }
 
             // Print the numbers in the current row
-            for (int j = 1; j <= stars; j++) {
+            for (int j = 1; j <= numberOfStars; j++) {
                 System.out.print(currentValue + " ");
             }
 
             // Update variables based on the current row
-            if (i < rows) {
-                spaces++;            // Increment spaces for the upper half
-                stars -= 2;          // Decrease stars for the upper half
-                currentValue--;      // Decrement the value to be printed
+            if (i < numberOfRows) {
+                numberOfSpaces++;            // Increment spaces for the upper half
+                numberOfStars -= 2;         // Decrease stars for the upper half
+                currentValue--;              // Decrement the value to be printed
             } else {
-                spaces--;            // Decrement spaces for the lower half
-                stars += 2;          // Increase stars for the lower half
-                currentValue++;      // Increment the value to be printed
+                numberOfSpaces--;            // Decrement spaces for the lower half
+                numberOfStars += 2;         // Increase stars for the lower half
+                currentValue++;              // Increment the value to be printed
             }
 
             // Move to the next line
