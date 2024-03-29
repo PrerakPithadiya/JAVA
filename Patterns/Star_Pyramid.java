306a4a07e46670d1a7f0729f49c69afc3b008fac
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2024-03-29 21:10:31 +0530

    Update Star_Pyramid.java

--- Patterns/Star_Pyramid.java
+++ Patterns/Star_Pyramid.java
@@ -2,14 +2,21 @@ package Patterns;
 
 public class Star_Pyramid {
     public static void main(String[] args) {
-        int n = 7;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i; j++) {
+        int numberOfRows = 7; // Number of rows (can be adjusted)
+
+        // Loop through each row
+        for (int i = 1; i <= numberOfRows; i++) {
+            // Print leading spaces
+            for (int j = 1; j <= numberOfRows - i; j++) {
                 System.out.print(" ");
             }
+
+            // Print stars for the current row
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
+
+            // Move to the next line
             System.out.println();
         }
     }
