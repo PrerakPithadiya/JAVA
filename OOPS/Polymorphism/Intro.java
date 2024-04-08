c839d5661541f6430edc305992e65e830a054002
Prerak Pithadiya <prerakpithadiya@gmail.com>
2024-04-08 23:53:07 +0530

    Update Intro.java

--- OOPS/Polymorphism/Intro.java
+++ OOPS/Polymorphism/Intro.java
@@ -45,7 +45,6 @@ public class Intro {
         void poly(Aeroplane ref) {
             ref.takeOff();
             ref.fly();
-            System.out.println("--------------------------------------");
         }
     }
 
@@ -60,3 +59,26 @@ public class Intro {
         a.poly(a);
     }
 }
+
+// Explanation of the given code
+
+// 1. The code defines a package `OOPS.Polymorphism`.
+
+// 2. Inside this package, there's a class named `Intro`, which contains several nested static classes representing different types of airplanes: `Aeroplane`, `Cargoplane`, `Passengerplane`, `Fighterplane`, and `Airport`.
+
+// 3. Each type of airplane (subclasses of `Aeroplane`) has two methods: `takeOff()` and `fly()`. These methods are overridden in the subclasses to provide specific implementations.
+
+// 4. The `Airport` class also contains a method named `poly(Aeroplane ref)`. This method takes an object of type `Aeroplane` as a parameter and demonstrates polymorphic behavior by calling the `takeOff()` and `fly()` methods on the passed object.
+
+// 5. In the `main` method:
+
+//    - Objects of type `Cargoplane`, `Passengerplane`, `Fighterplane`, and `Airport` are instantiated.
+//    - The `poly` method of the `Airport` object `a` is called multiple times with different types of airplanes (`Cargoplane`, `Passengerplane`, `Fighterplane`) and with itself (`Airport`). This demonstrates polymorphism, where the method behaves differently based on the type of object passed to it.
+
+// 6. When `poly` is called with a `Cargoplane` object, it prints "this is cargoplane takeoff" and "this is cargoplane fly".
+   
+// 7. When `poly` is called with a `Passengerplane` object, it prints "this is passengerplane takeoff" and "this is passengerplane fly".
+
+// 8. When `poly` is called with a `Fighterplane` object, it prints "this is fighterplane takeoff" and "this is fighterplane fly".
+
+// 9. When `poly` is called with an `Airport` object, it prints "this is aeroplane takeoff" and "this is aeroplane fly". This happens because `Airport` is a subclass of `Aeroplane` and it inherits the `takeOff()` and `fly()` methods from `Aeroplane`. However, it's not typically a good practice to include an `Airport` in the hierarchy of airplanes as it's not an airplane itself, but for the sake of demonstrating polymorphism, it serves the purpose.
