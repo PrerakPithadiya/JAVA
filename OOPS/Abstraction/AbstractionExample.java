07ea1a9026db888aba8fc98728613ea94fcddcd1
Prerak Pithadiya <prerakpithadiya@gmail.com>
2024-04-13 18:48:38 +0530

    Update AbstractionExample.java

--- OOPS/Abstraction/AbstractionExample.java
+++ OOPS/Abstraction/AbstractionExample.java
@@ -20,3 +20,8 @@ public class Main {
         myDog.makeSound();
     }
 }
+
+// - The Animal class is declared as abstract, containing an abstract method makeSound(). Abstract methods are defined without implementation and must be overridden by concrete subclasses.
+// - The Dog class extends the Animal class and provides an implementation for the makeSound() method by overriding it.
+// - In the main() method, an object of the Dog class is created.
+// - The makeSound() method is called on the Dog object, which prints "Woof Woof!" demonstrating the polymorphic behavior where the appropriate method implementation is selected at runtime based on the actual object type.
