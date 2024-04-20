20f3fa438af4d277509dc85c38df037598f13c72
Prerak Pithadiya <prerakpithadiya@gmail.com>
2024-04-20 13:39:02 +0530

    Update 5 - Inner Class.java

--- OOPS/Interface/5 - Inner Class.java
+++ OOPS/Interface/5 - Inner Class.java
@@ -1,22 +1,34 @@
+// Define a package for the classes
 package OOPS.Interface;
 
-class A {
+// Define a class A
+class Container {
+    // Define a method named 'show' in class A
     public void show() {
         System.out.println("It's Show method");
     }
 
-    static class B {
+    // Define a non-static nested class B within class A
+    class InnerClass {
+        // Define a method named 'display' in class B
         public void display() {
             System.out.println("It's Display method");
         }
     }
 }
 
-class InnerClass {
+// Define a class to demonstrate nested classes
+class NestedClassDemo {
+    // Main method
     public static void main(String[] args) {
-        A obj = new A();
-        obj.show();
-        A.B obj2 = new A.B();
-        obj2.display();
+        // Create an instance of the outer class A
+        Container outerObj = new Container();
+        // Call the show method of the outer class
+        outerObj.show();
+
+        // Create an instance of the non-static nested class B using the outer object
+        Container.InnerClass innerObj = outerObj.new InnerClass();
+        // Call the display method of the non-static nested class
+        innerObj.display();
     }
 }
