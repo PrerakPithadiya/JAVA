6dc8a36f76a8bd280787573383bf4b97ac8c5f51
Prerak Pithadiya <prerakpithadiya@gmail.com>
2024-04-27 20:05:02 +0530

    Add files via upload

--- OOPS/Exception Handling/1 - Arithmetic Exception Demo.java
+++ OOPS/Exception Handling/1 - Arithmetic Exception Demo.java
@@ -1,1 +1,14 @@
+package OOPS.Exception_Handling;
 
+class ArithmeticExceptionDemo {
+    public static void main(String[] args) {
+        try {
+            int dividend = 10;
+            int divisor = 0;
+            int result = dividend / divisor; // Attempting to divide by zero
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Exception caught: Division by zero!");
+        }
+    }
+}
