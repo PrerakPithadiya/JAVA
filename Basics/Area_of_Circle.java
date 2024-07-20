4da98fca7e98a1d64ba0dd66d3bcabf1e7034445
Prerak Pithadiya <prerakpithadiya@gmail.com>
2024-07-21 02:58:58 +0530

    make changes

--- Basics/Area_of_Circle.java
+++ Basics/Area_of_Circle.java
@@ -1,17 +1,32 @@
 import java.util.Scanner;
 
-public class Area_of_Circle {
+/**
+ * A program to calculate the area of a circle.
+ */
+public class AreaOfCircle {
+    /**
+     * The main method that prompts the user to enter the radius of a circle and
+     * prints the calculated area.
+     * 
+     * @param args command-line arguments (not used)
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the Radios of Circle : ");
+        System.out.print("Enter the Radius of Circle: ");
         float r = sc.nextFloat();
-        System.out.println("The Area of Circle is : " + calculateArea(r));
+        System.out.println("The Area of Circle is: " + calculateArea(r));
         sc.close();
     }
 
+    /**
+     * Calculates the area of a circle given its radius.
+     * 
+     * @param r the radius of the circle
+     * @return the area of the circle
+     * 
+     * Example: calculateArea(5.0f) returns approximately 78.53982
+     */
     public static float calculateArea(float r) {
-        float pI = 3.1415f, area = 0.0f;
-        area = pI * r * r;
-        return area;
+        return (float) (Math.PI * r * r);
     }
 }
