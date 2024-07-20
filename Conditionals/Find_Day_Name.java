82d3045b681ca582bd5f3096bec9beaace111bc8
PrerakPithadiya <prerakpithadiya@gmail.com>
2024-07-20 18:56:50 +0530

    "Refactored Find_Day_Name.java to use a switch statement and renamed the class to DayOfWeek"

--- Conditionals/Find_Day_Name.java
+++ Conditionals/Find_Day_Name.java
@@ -1,12 +1,43 @@
 import java.util.Scanner;
-public class Find_Day_Name{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the Day Number : ");
-        int day = sc.nextInt();
-        String result = findDayName(day);
-    }
-    public static String findDayName(int day){
-    
+
+class DayOfWeek {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the day number (1-7): ");
+        int dayNumber = scanner.nextInt();
+
+        String dayName;
+
+        switch (dayNumber) {
+            case 1:
+                dayName = "Monday";
+                break;
+            case 2:
+                dayName = "Tuesday";
+                break;
+            case 3:
+                dayName = "Wednesday";
+                break;
+            case 4:
+                dayName = "Thursday";
+                break;
+            case 5:
+                dayName = "Friday";
+                break;
+            case 6:
+                dayName = "Saturday";
+                break;
+            case 7:
+                dayName = "Sunday";
+                break;
+            default:
+                dayName = "Invalid day number. Please enter a number between 1 and 7.";
+                break;
+        }
+
+        System.out.println(dayName);
+
+        scanner.close();
     }
 }
